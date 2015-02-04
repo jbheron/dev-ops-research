62d11d1bccaf83b6e868dafa10eccbea71baf1db
Jess B Heron <jessbheron@gmail.com>
2015-02-03 16:18:55 -0800

    Update mine_sweeper_refactor.rb

--- mine_sweeper_refactor.rb
+++ mine_sweeper_refactor.rb
@@ -22,27 +22,25 @@ class MineSweeperBoardstateModel
     end
   end
 
-    def populate_neighbor_mine_count
-      @board.each_with_index do |row, row_index|
-        row.each_with_index do |element, i|
-          if element != "*"
-            neighbor_bomb_count = 0
-            neighbor_bomb_count += 1 if @board[row_index - 1][i - 1] == "*" unless i == 0 || row_index == 0 #check up_and_left
-            neighbor_bomb_count += 1 if @board[row_index - 1][i] == "*" unless row_index == 0 #check up
-            neighbor_bomb_count += 1 if @board[row_index - 1][i + 1] == "*" unless row_index == 0 #check up_and_right
-            neighbor_bomb_count += 1 if @board[row_index][i - 1] == "*" unless i == 0 #check left
-            neighbor_bomb_count += 1 if @board[row_index][i + 1] == "*" #check right
-            neighbor_bomb_count += 1 if @board[row_index + 1][i - 1] == "*" unless i == 0 || row_index == @board.count - 1 #check down_and_left
-            neighbor_bomb_count += 1 if @board[row_index + 1][i] == "*" unless row_index == @board.count - 1 #check down
-            neighbor_bomb_count += 1 if [row_index + 1][i + 1] == "*" unless row_index == @board.count - 1 #check down_and_right
-            @board[row_index][i] = neighbor_bomb_count if neighbor_bomb_count > 0 #insert count or whitespace
-          end
+  def populate_neighbor_mine_count
+    @board.each_with_index do |row, row_index|
+      row.each_with_index do |element, i|
+        if element != "*"
+          neighbor_bomb_count = 0
+          neighbor_bomb_count += 1 if @board[row_index - 1][i - 1] == "*" unless i == 0 || row_index == 0 #check up_and_left
+          neighbor_bomb_count += 1 if @board[row_index - 1][i] == "*" unless row_index == 0 #check up
+          neighbor_bomb_count += 1 if @board[row_index - 1][i + 1] == "*" unless row_index == 0 #check up_and_right
+          neighbor_bomb_count += 1 if @board[row_index][i - 1] == "*" unless i == 0 #check left
+          neighbor_bomb_count += 1 if @board[row_index][i + 1] == "*" #check right
+          neighbor_bomb_count += 1 if @board[row_index + 1][i - 1] == "*" unless i == 0 || row_index == @board.count - 1 #check down_and_left
+          neighbor_bomb_count += 1 if @board[row_index + 1][i] == "*" unless row_index == @board.count - 1 #check down
+          neighbor_bomb_count += 1 if [row_index + 1][i + 1] == "*" unless row_index == @board.count - 1 #check down_and_right
+          @board[row_index][i] = neighbor_bomb_count if neighbor_bomb_count > 0 #insert count or whitespace
         end
       end
-
+    end
   end
 
-  # ======= View
   def view_board
     @board[0].count.times { print "---"}; puts "--"
     @board.map do |row|
@@ -51,10 +49,8 @@ class MineSweeperBoardstateModel
     end
     @board[0].count.times { print "---"}; puts "--"
   end
-  # ==== End View
 
 end
-
 newboard = MineSweeperBoardstateModel.new(1, 3, 3)
 newboard2 = MineSweeperBoardstateModel.new(15, 12, 12)
 newboard3 = MineSweeperBoardstateModel.new(100, 40, 20)
